3fce62902b8e405d80814260962df79f979d6613
Matthew <meckart@perscholas.org>
2024-03-28 13:11:40 -0400

    Update Curriculum for Test

--- _meta.ts
+++ _meta.ts
@@ -1,7 +1,11 @@
 export default {
-  modules: {
+  302: {
+    title: "Verison Control with Git",
     type: "page",
-    title: "Modules",
+  },
+  308: {
+    title: "Introduction to JavaScript",
+    type: "page",
   },
   README: {
     display: "hidden",

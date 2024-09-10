b579495c1d3bd6fc4d64eb3391be64a0fbb0264d
Matthew <meckart@perscholas.org>
2024-09-10 12:00:04 -0400

    Meta Formatting

--- 302/_meta.ts
+++ 302/_meta.ts
@@ -1,1 +1,23 @@
-export default {};
+export default {
+  index: {},
+  "---": {
+    type: "separator",
+  },
+  "lesson-1": {},
+  "lesson-2": {},
+  "lesson-3": {},
+  "lesson-4": {},
+  "----": {
+    type: "separator",
+  },
+  "lab-1": {},
+  "-----": {
+    type: "separator",
+  },
+  "lesson-5": {},
+  "lesson-6": {},
+  "------": {
+    type: "separator",
+  },
+  sba: {},
+};
--- _meta.ts
+++ _meta.ts
@@ -1,4 +1,12 @@
 export default {
+  "*": {
+    theme: {
+      breadcrumb: false,
+      footer: false,
+      search: false,
+      typesetting: "article",
+    },
+  },
   302: {
     title: "Verison Control Primer",
     type: "page",

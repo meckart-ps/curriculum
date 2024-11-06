b7fc97d2fd7f2c489d41322ef14a0701053104d3
Matthew <meckart@perscholas.org>
2024-11-06 10:56:04 -0500

    Add M410

---
+++ 410/_meta.ts
@@ -0,0 +1,17 @@
+export default {
+  index: {},
+  "---": {
+    type: "separator",
+  },
+  "lesson-1": {},
+  "lesson-2": {},
+  "----": {
+    type: "separator",
+  },
+  "lab-1": {},
+  "-----": {
+    type: "separator",
+  },
+  "lesson-3": {},
+  sba: {},
+};

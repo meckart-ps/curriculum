2bd46364b58dfdc64de6d6b65acf1615b070a3fe
Matthew <meckart@perscholas.org>
2024-11-08 10:04:26 -0500

    M412 Skeleton

---
+++ 412/_meta.ts
@@ -0,0 +1,27 @@
+export default {
+  index: {},
+  "---": {
+    type: "separator",
+  },
+  "lesson-1": {},
+  "lesson-2": {},
+  "lesson-3": {},
+  "----": {
+    type: "separator",
+  },
+  "lab-1": {},
+  "-----": {
+    type: "separator",
+  },
+  "lesson-4": {},
+  "lesson-5": {},
+  "lesson-6": {},
+  "------": {
+    type: "separator",
+  },
+  "lab-2": {},
+  "-------": {
+    type: "separator",
+  },
+  sba: {},
+};

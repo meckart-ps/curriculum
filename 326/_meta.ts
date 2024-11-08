c3bcf968f701d8dbf0073adc66128654b317d5c2
Matthew <meckart@perscholas.org>
2024-11-07 23:14:15 -0500

    Create M326

---
+++ 326/_meta.ts
@@ -0,0 +1,30 @@
+export default {
+  index: {},
+  "---": {
+    type: "separator",
+  },
+  "lesson-1": {},
+  "lesson-2": {},
+  "lesson-3": {},
+  "lesson-4": {},
+  "----": {
+    type: "separator",
+  },
+  "lab-1": {},
+  "-----": {
+    type: "separator",
+  },
+  "lesson-5": {},
+  "lab-2": {},
+  "------": {
+    type: "separator",
+  },
+  "lesson-6": {},
+  "lesson-7": {},
+  "lesson-8": {},
+  "lab-3": {},
+  "-------": {
+    type: "separator",
+  },
+  sba: {},
+};
